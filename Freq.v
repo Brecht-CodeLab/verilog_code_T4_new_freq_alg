1a12b3fdf1581b33b5c58516e95be6a9d587c607
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-04-15 11:34:32 +0200

    try with tasks included in module

--- Freq.v
+++ Freq.v
@@ -17,6 +17,35 @@ module Freq(
     reg [11:0] highestADC;
     reg freqAlgStarted;
 
+
+    task UpdateFreq(inout freqAlgStarted, input [19:0] freq, output freqAlgDone, [19:0] newFreq);
+        if(freqAlgStarted == 1 && freq < 20'hAFC8)begin
+            newFreq <= freq + 20'h32;
+        end
+        else if (freq > 20'hAFC7) begin
+            freqAlgDone <= 1;
+        end
+        else begin
+            newFreq <= 20'h88B8; //35kHz
+            freqAlgStarted <= 1;
+        end
+    endtask
+
+
+        task CheckHighestADC(input [11:0] ADC, [19:0] freq, output [19:0] bestFreq, inout [11:0] highestADC);
+        if(ADC < 12'h800 && ADC > highestADC)begin
+            highestADC <= ADC;
+            bestFreq <= freq;
+        end
+        else if(ADC >= 12'h800 && 12'hFFF - ADC > highestADC)begin
+            highestADC <= 12'hFFF - ADC;
+            bestFreq <= freq;
+        end
+    endtask
+
+
+
+
     initial begin
         startupBuffer = 24'h30D40; //2ms
         freqSwitchBuffer = 24'h30D40; //2ms
@@ -27,7 +56,7 @@ module Freq(
         freqAlgDone = 0;
     end
 
-    `include "tasks.v"
+    //`include "tasks.v"
 
     always @(clk) begin
         if (~nrst || ~swiptAlive) begin

5fe8a6590b6c76444ce62f3cbdb47179adb04505
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-04-15 12:23:30 +0200

    bug in freq

--- Freq.v
+++ Freq.v
@@ -18,34 +18,6 @@ module Freq(
     reg freqAlgStarted;
 
 
-    // task UpdateFreq(inout freqAlgStarted, input [19:0] freq, output freqAlgDone, output [19:0] newFreq);
-    //     if(freqAlgStarted == 1 && freq < 20'hAFC8)begin
-    //         newFreq <= freq + 20'h32;
-    //     end
-    //     else if (freq > 20'hAFC7) begin
-    //         freqAlgDone <= 1;
-    //     end
-    //     else begin
-    //         newFreq <= 20'h88B8; //35kHz
-    //         freqAlgStarted <= 1;
-    //     end
-    // endtask
-
-
-    //     task CheckHighestADC(input [11:0] ADC, input [19:0] freq, output [19:0] bestFreq, inout [11:0] highestADC);
-    //     if(ADC < 12'h800 && ADC > highestADC)begin
-    //         highestADC <= ADC;
-    //         bestFreq <= freq;
-    //     end
-    //     else if(ADC >= 12'h800 && 12'hFFF - ADC > highestADC)begin
-    //         highestADC <= 12'hFFF - ADC;
-    //         bestFreq <= freq;
-    //     end
-    // endtask
-
-    `include "tasks.v"
-
-
     initial begin
         startupBuffer = 24'h30D40; //2ms
         freqSwitchBuffer = 24'h30D40; //2ms
@@ -55,7 +27,8 @@ module Freq(
         freqAlgStarted = 0;
         freqAlgDone = 0;
     end
-
+    
+    `include "tasks.v"
 
     always @(posedge clk) begin
         if (~nrst || ~swiptAlive) begin
@@ -79,6 +52,9 @@ module Freq(
             if (freqSwitchBuffer == 0) begin
                 freqSwitchBuffer <= 24'h30D40;
                 UpdateFreq(freqAlgStarted, freq, freqAlgDone, newFreq);
+            end
+            else begin
+                freqSwitchBuffer <= freqSwitchBuffer - 1;
             end
             CheckHighestADC(ADC, freq, bestFreq, highestADC);
         end

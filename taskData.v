604a5f3988c429994691a1d2a52518f792102acb
Brecht-CodeLab <brecht.vandekerkhove@student.kuleuven.be>
2021-04-16 11:10:45 +0200

    bug fix

--- taskData.v
+++ taskData.v
@@ -55,37 +55,36 @@ task GetDataFromZybo;
     else begin
         getDataFromZybo <= 0;
         getMeanCurrent <= 0;
+        case (mode)
+            00:dataFromZybo <= 16'b1010101010101010;
+            01:begin
+                case (type)
+                    00:dataFromZybo <= SWIPT_P_TX;
+                    01:dataFromZybo <= SWIPT_DUTY;
+                    10:dataFromZybo <= SWIPT_FREQ;
+                    11:dataFromZybo <= SWIPT_ASCII;
+                    default:dataFromZybo <= 16'b1010101010101010;
+                endcase
+            end
+            10:begin
+                case (type)
+                    00:dataFromZybo <= ANC_MAX_HEIGHT;
+                    01:dataFromZybo <= ANC_MIN_HEIGHT;
+                    10:dataFromZybo <= 16'b1010101010101010;
+                    11:dataFromZybo <= 16'b1010101010101010;
+                    default:dataFromZybo <= 16'b1010101010101010;
+                endcase
+            end
+            11:begin
+                case (type)
+                    00:dataFromZybo <= COMMS_TRAJECT;
+                    01:dataFromZybo <= COMMS_QR_CODES;
+                    10:dataFromZybo <= COMMS_FLIGHT_TIME;
+                    11:dataFromZybo <= 16'b1010101010101010;
+                    default:dataFromZybo <= 16'b1010101010101010;
+                endcase
+            end
+            default:dataFromZybo <= 16'b1010101010101010;
+        endcase
     end
-
-    case (mode)
-        00:dataFromZybo <= 16'b1010101010101010;
-        01:begin
-            case (type)
-                00:dataFromZybo <= SWIPT_P_TX;
-                01:dataFromZybo <= SWIPT_DUTY;
-                10:dataFromZybo <= SWIPT_FREQ;
-                11:dataFromZybo <= SWIPT_ASCII;
-                default:dataFromZybo <= 16'b1010101010101010;
-            endcase
-        end
-        10:begin
-            case (type)
-                00:dataFromZybo <= ANC_MAX_HEIGHT;
-                01:dataFromZybo <= ANC_MIN_HEIGHT;
-                10:dataFromZybo <= 16'b1010101010101010;
-                11:dataFromZybo <= 16'b1010101010101010;
-                default:dataFromZybo <= 16'b1010101010101010;
-            endcase
-        end
-        11:begin
-            case (type)
-                00:dataFromZybo <= COMMS_TRAJECT;
-                01:dataFromZybo <= COMMS_QR_CODES;
-                10:dataFromZybo <= COMMS_FLIGHT_TIME;
-                11:dataFromZybo <= 16'b1010101010101010;
-                default:dataFromZybo <= 16'b1010101010101010;
-            endcase
-        end
-        default:dataFromZybo <= 16'b1010101010101010;
-    endcase
 endtask
